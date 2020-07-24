b3465656345493edc34f3676e2fe5b3c4d67cada
jessekim-ck <jessekim.ck.94@gmail.com>
2020-07-25 00:45:07 +0900

    Initial commit

---
+++ install_docker.sh
@@ -0,0 +1,19 @@
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update && sudo apt-cache search docker-ce
+# Message: docker-ce - Docker: the open-source application container engine
+
+sudo apt-get update && sudo apt-get install docker-ce
+sudo usermod -aG docker $USER
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get isntall -y nvidia-container-toolkit
+sudo systemctl restart docker
+
+echo "Please reboot the machine."
---
+++ install_utils.sh
@@ -0,0 +1,14 @@
+# Necessary packages
+sudo apt-get update
+sudo apt-get install vim git curl openssh-server
+
+# The Ultimate vimrc
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# Oh-my-zsh
+sudo apt-get install zsh
+sudo curl -L http://install.ohmyz.sh | sh
+sudo chsh $USER -s /usr/bin/zsh
+
+echo "Please reboot to apply oh-my-zsh."
